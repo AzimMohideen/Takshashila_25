297871489c8d080a2c1d79ccf0d8f56c6da75c7d
Ajaybalajiprasad <ajaybalajiprasad02@gmail.com>
2025-02-25 13:08:28 +0530

    fix: Disable speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from 'next/font/local';
 import "./globals.css";
 import { StyledToastContainer } from '@/components/CassetteToast';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
 
 const lexendExa = localFont({
@@ -42,7 +41,6 @@ export default function RootLayout({
       <body className={`${lexendExa.variable} ${lexendDeca.variable} antialiased overflow-x-hidden`}>
         {children}
         <StyledToastContainer />
-        <SpeedInsights/>
         <Analytics/>
       </body>
     </html>
